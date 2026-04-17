d0b4029ad6d56eb7371726362355b81168d93600
𝗦𝗣𝗬-𝗘 <161654922+123tool@users.noreply.github.com>
2026-04-17 07:02:22 +0700

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+// KONFIGURASI TRIPAY
+define('TRIPAY_API_KEY', 'MASUKKAN_API_KEY_KAMU');
+define('TRIPAY_PRIVATE_KEY', 'MASUKKAN_PRIVATE_KEY_KAMU');
+define('TRIPAY_MERCHANT_CODE', 'Txxxx'); // Kode Merchant
+
+// KONFIGURASI NOTIFIKASI
+define('TELE_TOKEN', 'TOKEN_BOT_TELEGRAM');
+define('TELE_CHAT_ID', 'ID_CHAT_KAMU');
+
+// KONFIGURASI DATABASE (Opsional)
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "spye_database";
+?>
